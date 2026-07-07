c32db68ce5ff018b889d27e5cf20b9842bb7473f
ohyo <khr157929@gmail.com>
2026-07-07 21:39:18 +0900

    마운트시에 다크모드 체크

--- src/.vuepress/components/NaiveProvider.vue
+++ src/.vuepress/components/NaiveProvider.vue
@@ -1,12 +1,17 @@
 <script setup lang="ts">
 import { NConfigProvider, darkTheme } from "naive-ui";
+import { onMounted, ref } from "vue";
 import { useDarkMode } from "vuepress-theme-hope/client";
 
-const isDark = useDarkMode();
+const isDark = ref(false);
+
+onMounted(() => {
+  isDark.value = useDarkMode().isDarkMode.value;
+});
 </script>
 
 <template>
-  <n-config-provider :theme="isDark.isDarkMode.value ? darkTheme : null">
+  <n-config-provider :theme="isDark ? darkTheme : null">
     <slot />
   </n-config-provider>
 </template>
