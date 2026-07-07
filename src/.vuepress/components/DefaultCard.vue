0434c19475cabe82e5b29a0fb9f9cf711eb53b70
ohyo <khr157929@gmail.com>
2026-07-07 17:53:30 +0900

    Get title

--- src/.vuepress/components/DefaultCard.vue
+++ src/.vuepress/components/DefaultCard.vue
@@ -1,6 +1,14 @@
+<script setup lang="ts">
+const props = defineProps({
+  title: {
+    type: String,
+  },
+});
+</script>
+
 <template>
   <naive-provider>
-    <n-card size="large">
+    <n-card size="large" :title="props.title">
       <slot />
     </n-card>
   </naive-provider>
