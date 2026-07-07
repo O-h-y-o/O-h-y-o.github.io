266bb2939c8a4605c8345132a049c1ad37a4d219
ohyo <khr157929@gmail.com>
2026-07-07 21:44:03 +0900

    동적할당

--- src/.vuepress/components/NaiveProvider.vue
+++ src/.vuepress/components/NaiveProvider.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
 import { NConfigProvider, darkTheme } from "naive-ui";
-import { onMounted, ref } from "vue";
+import { onMounted, ref, watch } from "vue";
 import { useDarkMode } from "vuepress-theme-hope/client";
 
 const isDark = ref(false);
+
+watch(useDarkMode().isDarkMode, (newVal) => {
+  isDark.value = newVal;
+});
 
 onMounted(() => {
   isDark.value = useDarkMode().isDarkMode.value;
